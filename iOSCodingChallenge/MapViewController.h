580a49dfeff85b0122291a5f6311d24603cfaf7a
Kesh Pola <kishvarsho.pallaev@gmail.com>
2017-10-22 22:34:15 +0600

    add bridging file

---
+++ iOSCodingChallenge/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  iOSCodingChallenge
+//
+//  Created by Kesh Pola on 10/22/17.
+//  Copyright © 2017 KeshPola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ iOSCodingChallenge/MapViewController.m
@@ -0,0 +1,22 @@
+//
+//  MapViewController.m
+//  iOSCodingChallenge
+//
+//  Created by Kesh Pola on 10/22/17.
+//  Copyright © 2017 KeshPola. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
